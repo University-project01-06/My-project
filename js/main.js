fe00204a1f97800dbea8d915c51072990035eed1
Yaroslav Tsybukh <yaroslavcybuh26092001@gmail.com>
2020-06-01 19:58:16 +0300

    new project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.slider').slick({
+        speed:500,
+        dots:true,
+        waitForAnimate:false,
+        infinite:true,
+        autoplay:true,
+        draggable:false,
+        autoplaySpeed:1000,
+        pauseonFocus:true,
+    });
+})
+
+   
+
